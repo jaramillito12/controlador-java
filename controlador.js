10c2c09e9c3d5d31fb198f40f367120e762933ef
jaramillo <jaramillito098x@gmail.com>
2022-03-30 16:00:05 -0500

    index.html

---
+++ controlador.js
@@ -0,0 +1,39 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click", function(event){
+    let selector=document.getElementById("selector").value
+    alert("usted eligio el:" + selector)
+    switch(selector){
+        case "1":
+        alert("elegiste el ·1")
+        break
+        case "2":
+        alert("elegiste el ·2")
+        break
+        case "3":
+        alert("elegiste el ·3")
+        break
+        case "4":
+        alert("elegiste el ·4")
+        break
+        case "5":
+        alert("elegiste el ·5")
+        break
+        case "6":
+        alert("elegiste el ·6")
+        break
+        case "7":
+        alert("elegiste el ·7")
+        break
+        case "8":
+        alert("elegiste el ·8")
+        break
+        case "9":
+        alert("elegiste el ·9")
+        break
+        case "10":
+        alert("elegiste el ·10")
+        break
+        default:
+            alert("opcion no valida")
+    }
+})
